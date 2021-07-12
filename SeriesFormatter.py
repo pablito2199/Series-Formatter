07a2268204010e5ca297bad94e259f1b7c1aac44
Pablo Tarrío Otero <63094934+pablito2199@users.noreply.github.com>
2021-07-12 12:40:47 +0200

    Better performance

--- SeriesFormatter.py
+++ SeriesFormatter.py
@@ -1,13 +1,14 @@
 from pathlib import Path
 
-path = input("Which is the path to the series' files? ")
-serie = input("Which is the series' name? ")
-season = input("Which is the season from " + serie +  "? ")
-extension = input("Which is your file extension? (e.g: mp4) ")
+path = input("Enter the path to the series' files: ")
+serie = input("Enter the series' name: ")
+season = input(f"Enter the season from {serie}: ")
+extension = input("Enter the file extension (e.g: mp4): ")
 chapter = 1
 
 for file in Path(path).iterdir():
     if file.is_file():
         new_name = serie + " S" + season.zfill(2) + "E" + str(chapter).zfill(2) + "." + extension
         file.rename(Path(path, new_name))
+        print(file.stem + " --> " + new_name)
         chapter += 1
